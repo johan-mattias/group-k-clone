dfb900469297f48415e0d1286b77496424fab1e9
Fredrik Sandelin <fredriksandelin94@gmail.com>
2018-02-02 22:32:19 +0100

    Network handler on server

--- run_client.py
+++ run_client.py
@@ -2,14 +2,15 @@ from network import TcpHandler, UdpHandler
 from snider_glider import Game, ClientGUI, Player
 import threading, queue
 
+SERVER_TCP_ADDRESS = ("antoncarlsson.se", 12000)
+
 class NetworkHandler(threading.Thread):
-    def __init__(self, threadID, name, udp_handler, tcp_handler, data_queue):
+    def __init__(self, data_queue):
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.udp_handler = udp_handler
-        self.tcp_handler = tcp_handler
+        self.udp_handler = UdpHandler.UdpHandler()
+        self.tcp_handler = TcpHandler.TcpHandler(SERVER_TCP_ADDRESS)
         self.data_queue = data_queue
+
 
     def run(self):
         print("initializing udp receiving")
@@ -24,21 +25,15 @@ class NetworkHandler(threading.Thread):
             print("X: " + str(x_pos) + " - Y:" + str(y_pos))
 
 
-SERVER_TCP_ADDRESS = ("antoncarlsson.se", 12000)
-
-
 def main():
-    udp_handler = UdpHandler.UdpHandler()
-    tcp_handler = TcpHandler.TcpHandler(SERVER_TCP_ADDRESS)
-
     data_queue = queue.Queue(100)
 
-    network_handler = NetworkHandler(1, "Network", udp_handler, tcp_handler, data_queue)
-    game = Game.Game(2, "Game client", data_queue, demo_player=True)
+    network_handler = NetworkHandler(data_queue)
+    #game = Game.Game(2, "Game client", data_queue, demo_player=True)
 
 
     network_handler.start()
-    game.start()
+    #game.start()
 
 
 if __name__ == '__main__':
--- run_server.py
+++ run_server.py
@@ -1,18 +1,86 @@
 from network import utils, TcpHandler, UdpHandler
-import math, time
+import math, time, threading
 
-tcp = TcpHandler.TcpHandler()
-udp = UdpHandler.UdpHandler()
+class NetworkHandler(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.main_tcp_handler = TcpHandler.TcpHandler()#Should be port 12000
+        print(self.main_tcp_handler.port)
+        self.udp_handler_listener = UdpHandler.UdpHandler()
+        self.udp_handler_sender = UdpHandler.UdpHandler()
 
-print("waiting for TCP conn")
-address = tcp.accept()
-port = tcp.receive()
-port = int(port.decode())
+    def run(self):
+        self.udp_thread = UdpThread(self.udp_handler_listener)
+        self.main_tcp_thread = MainTcpThread(self.main_tcp_handler, self.udp_thread)
+        self.main_tcp_thread.start()
+        self.udp_thread.start()
+        
+class MainTcpThread(threading.Thread):
+    def __init__(self, tcp_handler, udp_thread):
+        threading.Thread.__init__(self)
+        self.tcp_handler = tcp_handler
+        self.udp_thread = udp_thread
 
-angle = 0
+    def run(self):
+        remote_address = self.tcp_handler.accept()
+        new_tcp_handler = TcpHandler.TcpHandler()
+        new_tcp_thread = TcpThread(new_tcp_handler, remote_address[0])
+        new_port = new_tcp_thread.port
+        new_tcp_thread.start()
+        #TODO
+        #check auth
+        self.udp_thread.add_accepted_ip((remote_address[0], None))
+        #Send new_port to client
+        #close connection
+        #accept new connection
 
-while True:
-    x, y = math.cos(angle)*50 + 200, math.sin(angle)*50 + 200
-    angle += math.pi/30
-    udp.send((address[0], port), (utils.unixtime(), int(x), int(y)))
-    time.sleep(1/60)
+class TcpThread(threading.Thread):
+    def __init__(self, tcp_handler, remote_ip):
+        threading.Thread.__init__(self)
+        self.tcp_handler = tcp_handler
+        self.remote_ip = remote_ip
+
+    def run(self):
+        remote_address = self.tcp_handler.accept()
+        #TODO if(remote_address[0] != remote_ip): handle
+        print("Connection from", remote_address[0], "accepted")
+        print("Expected from", self.remote_ip)
+        
+class UdpThread(threading.Thread):
+    def __init__(self, udp_handler):
+        threading.Thread.__init__(self)
+        self.udp_handler = udp_handler
+        self.address_list = list()
+        self.ip_without_port = list()
+
+    def run(self):
+        while True:
+            #receive
+            address, data = self.udp_handler.receive()
+            add_port(address)
+            print(data)
+            #send
+            x, y = math.cos(angle)*50 + 200, math.sin(angle)*50 + 200
+            angle += math.pi/30
+            for address in self.address_list:
+                if(address[1] != None):
+                    self.udp_handler.send((address), (utils.unixtime(), int(x), int(y)))
+            time.sleep(1/60)
+                
+    def add_accepted_ip(address):
+        self.address_list.append(address)
+        self.ip_without_port.append(address[0])
+
+    def add_port(address):
+        for ip in self.ip_without_port:
+            if(address[0] == ip):
+                for i in len(self.address_list):
+                    if (address[0] == self.address_list[i][0]):
+                        self.address_list[i][1] = address[1]
+                        self.ip_without_port.remove(ip)
+                        return
+
+network = NetworkHandler()
+network.start()
+
+
