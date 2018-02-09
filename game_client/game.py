15c180c1d86f0f424a4fb2d40bf732e98ee3419f
Anton Carlsson <mail@antoncarlsson.se>
2018-02-09 17:22:08 +0100

    Created a simple game client where the player can move left and right and jump. Other players can be created and their positions can be updated by calling the player method network_update(x, y).

---
+++ game_client/game.py
@@ -0,0 +1,51 @@
+import pyglet
+from player import Player
+from random import randint
+
+game_window = pyglet.window.Window(800, 600)
+
+# player_batch = pyglet.graphics.Batch()
+
+player_image = pyglet.resource.image('player.png')
+# player = pyglet.sprite.Sprite(player_image, x=400, y=300, batch=player_batch)
+# players = [player]
+
+
+def center_image(image):
+    image.anchor_x = image.width / 2
+    image.anchor_y = image.height / 2
+
+
+center_image(player_image)
+player = Player(player_image, x=0, y=0, batch=None)
+game_window.push_handlers(player)
+game_window.push_handlers(player.key_handler)
+
+player_batch = pyglet.graphics.Batch()
+player2 = Player(player_image, x=300, y=400, batch=player_batch)
+player3 = Player(player_image, x=400, y=300, batch=player_batch)
+player4 = Player(player_image, x=500, y=500, batch=player_batch)
+players = [player2, player3, player4]
+
+game_objects = [player] + players
+
+
+@game_window.event
+def on_draw():
+    game_window.clear()
+    for obj in game_objects:
+        obj.draw()
+
+
+def update(dt):
+    player.update(dt)
+
+    for p in players:
+        x = p.x + randint(-10, 10) * 10 * dt
+        y = p.y + randint(-10, 10) * 10 * dt
+        p.network_update(x, y)
+
+
+if __name__ == '__main__':
+    pyglet.clock.schedule_interval(update, 1 / 120.0)
+    pyglet.app.run()
---
+++ game_client/player.py
@@ -0,0 +1,54 @@
+import pyglet
+from pyglet.window import key
+
+
+class Player(pyglet.sprite.Sprite):
+    def __init__(self, *args, **kwargs):
+        super(Player, self).__init__(*args, **kwargs)
+
+        self.jumping = False
+        self.velocity_x, self.velocity_y = 400.0, 30.0
+        self.mass = 3
+        self.key_handler = key.KeyStateHandler()
+
+    def jump(self, dt):
+        if self.velocity_y > 0:
+            self.y += (0.5 * self.mass * (self.velocity_y ** 2)) * dt
+        else:
+            self.y += -(0.5 * self.mass * (self.velocity_y ** 2)) * dt
+
+        self.velocity_y -= 2
+
+    def update(self, dt):
+        if self.key_handler[key.A]:
+            self.x -= self.velocity_x * dt
+        if self.key_handler[key.D]:
+            self.x += self.velocity_x * dt
+        if self.key_handler[key.SPACE] and not self.jumping:
+            self.jumping = True
+
+        if self.jumping:
+            self.jump(dt)
+
+        self.check_bounds()
+
+    def network_update(self, x, y):
+        self.x = x
+        self.y = y
+
+    def check_bounds(self):
+        min_x = self.image.width / 2
+        min_y = self.image.height / 2
+        max_x = 800 - self.image.width / 2
+        max_y = 600 - self.image.height / 2
+
+        if self.x < min_x:
+            self.x = min_x
+        if self.x > max_x:
+            self.x = max_x
+        if self.y < min_y:
+            self.y = min_y
+            self.jumping = False
+            self.velocity_y = 30.0
+        if self.y > max_y:
+            self.y = max_y
