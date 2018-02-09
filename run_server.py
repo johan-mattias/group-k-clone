a4a41ffd721468a6a0b8ee5c9225013807a1781d
Fredrik Sandelin <fredriksandelin94@gmail.com>
2018-02-09 16:20:14 +0100

    fixed strange indent

--- run_server.py
+++ run_server.py
@@ -84,6 +84,7 @@ class TcpThread(threading.Thread):
             data_format, data = self.tcp_handler.receive()
             self.data_format_mapping[data_format](data)
         except:
+            pass
             #print("Socket timed out")
 
     def handle_player_update(self, data):
