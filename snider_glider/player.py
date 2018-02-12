14b166a40624b63785b8e6367e2aaced2cb4e9ce
Fredrik Sandelin <fredriksandelin94@gmail.com>
2018-02-12 13:57:03 +0100

    Deleted old files that are not in use. Moved files from game_client to snider_glider

--- snider_glider/player.py
+++ snider_glider/player.py
@@ -1,108 +1,59 @@
-import pickle
 import pyglet
+from pyglet.window import key
 
-class Player:
 
-    def __init__(self, player_id, user_id, name, keys, image):
-        self.player_id = player_id
+class Player(pyglet.sprite.Sprite):
+    def __init__(self, user_id, player_id, name, npc, *args, **kwargs):
+        super(Player, self).__init__(*args, **kwargs)
+
         self.user_id = user_id
-        
-        self.sprite_width = 50
-        self.sprite_height = 50
-
-        self.image = image
-        self.sprite = image
-        if image == 'testSprite.png':
-            image = pyglet.image.load('testSprite.png')
-            self.sprite = pyglet.sprite.Sprite(image) #seg fault
-            self.scale_sprite(self.sprite)
-            
-        self.movementSpeed = (0, 0)
-        self.vSpeed = 0
-        self.x, self.y = (0, 0)
-
-        self.name = name
-        self.controllable = False
-        if keys is not None:
-            self.up = keys[0]
-            self.right = keys[1]
-            self.down = keys[2]
-            self.left = keys[3]
-            self.controllable = True
-        self.move_rate = 1
-
-    def scale_sprite(self, sprite):
-        x_scale = self.sprite_width/sprite.width
-        y_scale = self.sprite_height/sprite.height
-
-        sprite.scale_x = x_scale
-        sprite.scale_y = y_scale
-        
-    def generate_movement_speed(self, keys):
-        m1 = (0, 0)
-        m2 = (0, 0)
-        m3 = (0, 0)
-        m4 = (0, 0)
-
-        if keys[self.up]:       m1 = (0, self.move_rate)
-        if keys[self.right]:    m2 = (self.move_rate, 0)
-        if keys[self.down]:     m3 = (0, -self.move_rate)
-        if keys[self.left]:     m4 = (-self.move_rate, 0)
-
-        self.movementSpeed = tuple(map(sum, zip(m1, m2, m3, m4)))
-
-    def draw_player(self, window):
-        self.sprite.position = self.get_position()
-        self.sprite.draw()
-    
-    def draw(self, py):
-        self.draw_player(py)
-
-    def move(self, keys):
-        self.generate_movement_speed(keys)
-
-    def set_position(self, x, y):
-        self.x = x
-        self.y = y
-
-    def get_position(self):
-        return self.x, self.y
-
-    def to_transfer_object(self):
-        return PlayerTO(self.player_id, self.user_id, x=self.x, y=self.y, x_velocity=self.movementSpeed[0], y_velocity=self.movementSpeed[1], name=self.name)
-
-    def __repr__(self):
-        return str(self)
-    
-    def __str__(self):
-        return "pid: " + str(self.player_id) + " name: " + str(self.name) +  " image: " + str(self.image)
-    
-class PlayerTO:
-    def __init__(self, player_id, user_id=None, x=None, y=None, x_velocity=None, y_velocity=None, color=None, name=None, image = None):
         self.player_id = player_id
-        self.user_id = user_id
-        self.x = x
-        self.y = y
-
-        self.x_velocity = x_velocity
-        self.y_velocity = y_velocity
-
-        self.color = color
         self.name = name
-        self.image = image
+        self.npc = npc
 
-    def serialize(self):
-        return pickle.dumps(self, protocol=pickle.HIGHEST_PROTOCOL)
-    
-    def __repr__(self):
-        return str(self)
-    
-    def __str__(self):
-        return "pid: " + str(self.player_id) + " x: " + str(self.x) + " y: " + str(self.y)
+        self.jumping = False
+        self.velocity_x, self.velocity_y = 400.0, 30.0
+        self.mass = 3
+        self.key_handler = key.KeyStateHandler()
 
-def player_from_player_to(p, image = None):
-    if image == None:
-        return Player(p.player_id, p.user_id, p.name, None, p.image)
-    else:
-        return Player(p.player_id, p.user_id, p.name, None, image)
-    
+    def jump(self, dt):
+        if self.velocity_y > 0:
+            self.y += (0.5 * self.mass * (self.velocity_y ** 2)) * dt
+        else:
+            self.y += -(0.5 * self.mass * (self.velocity_y ** 2)) * dt
+
+        self.velocity_y -= 2
+
+    def update(self, dt, x=0, y=0):
+        if self.npc:
+            self.x = x
+            self.y = y
+        else:
+            if self.key_handler[key.A]:
+                self.x -= self.velocity_x * dt
+            if self.key_handler[key.D]:
+                self.x += self.velocity_x * dt
+            if self.key_handler[key.SPACE] and not self.jumping:
+                self.jumping = True
+
+            if self.jumping:
+                self.jump(dt)
+
+        self.check_bounds()
+
+    def check_bounds(self):
+        min_x = self.image.width / 2
+        min_y = self.image.height / 2
+        max_x = 800 - self.image.width / 2
+        max_y = 600 - self.image.height / 2
+
+        if self.x < min_x:
+            self.x = min_x
+        if self.x > max_x:
+            self.x = max_x
+        if self.y < min_y:
+            self.y = min_y
+            self.jumping = False
+            self.velocity_y = 30.0
+        if self.y > max_y:
+            self.y = max_y
